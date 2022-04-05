cb301cc166e4767e7f4e17aa73e4c738a1ee365f
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-05 14:44:12 +0500

    Made some logic

--- HW2.3.1/LoginViewController.swift
+++ HW2.3.1/LoginViewController.swift
@@ -4,21 +4,39 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var userPasswordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let userName = "Homework"
+    private let password = "Password"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let userLogin = segue.destination as? WelcomeViewController else { return }
         
+        userLogin.nameOfUser = "Welcome, \(userName)!"
     }
     
     @IBAction func logButton() {
+        guard userNameTF.text == userName else { showAlert(); return }
+        guard userPasswordTF.text == password else { showAlert(); return }
+        
+        userNameTF.text?.removeAll()
+        userPasswordTF.text?.removeAll()
     }
     
     @IBAction func showNameButton() {
-        
+        showAlert(message: "Your login is \(userName)")
     }
     
     @IBAction func showPasswordButton() {
-        
+        showAlert(message: "Your password is \(password)")
     }
-    
 }
 
+//MARK: Notification method
+extension LoginViewController {
+    private func showAlert(title: String = "Information", message: String = "Incorrect login or password. Try again") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let oKAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(oKAction)
+        present(alert, animated: true)
+    }
+}
--- HW2.3.1/WelcomeViewController.swift
+++ HW2.3.1/WelcomeViewController.swift
@@ -3,12 +3,15 @@ import UIKit
 class WelcomeViewController: UIViewController {
     @IBOutlet var welcomeLabel: UILabel?
     
+    var nameOfUser: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        welcomeLabel?.text = nameOfUser
     }
     
     @IBAction func logOutButton() {
-        
+        dismiss(animated: true)
     }
 }
