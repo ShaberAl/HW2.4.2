cddd90fce8ee8fc06f4657eab693238fde37bba2
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-05 13:27:21 +0500

    Prepared for coding

---
+++ HW2.3.1/LoginViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var userPasswordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func logButton() {
+    }
+    
+    @IBAction func showNameButton() {
+        
+    }
+    
+    @IBAction func showPasswordButton() {
+        
+    }
+    
+}
+
---
+++ HW2.3.1/WelcomeViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet var welcomeLabel: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func logOutButton() {
+        
+    }
+}
